93164301c758cbfd732295961c681b4276693429
Nguyễn Trung Thắng <trungthangdk@gmail.com>
2019-04-27 01:44:29 +0700

    ug

--- WebVeTau/src/main/java/com/web/vetau/WebVeTauApplication.java
+++ WebVeTau/src/main/java/com/web/vetau/WebVeTauApplication.java
@@ -2,9 +2,10 @@ package com.web.vetau;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WebVeTauApplication {
+public class WebVeTauApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebVeTauApplication.class, args);
